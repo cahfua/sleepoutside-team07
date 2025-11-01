5e5434f8317ee66914b2f5fb94290bb671f5d566
Ederson <jvedrson@gmail.com>
2025-10-31 21:46:16 -0400

    fix addProductToCart to add items to the cart and avoid duplicate products in

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart") || [];
+  const existingProduct = cart.find((item) => item.Id === product.Id);
+  if (!existingProduct) {
+    cart.push(product);
+    setLocalStorage("so-cart", cart);
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
