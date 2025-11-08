2e33a56d4f213acf5f75019f0313bc173bf40672
kehnniey <kehnniey@gmail.com>
2025-11-08 11:02:59 -0500

    update

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -26,17 +26,7 @@ export function setLocalStorage(key, data) {
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// retrieve data from localstorage
-export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
-}
-// save data to local storage
-export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
