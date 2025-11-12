2d7008de3d1302d7f6675aa193384a326604721f
kehnniey <kehnniey@gmail.com>
2025-11-12 17:45:00 -0500

    dynamic footer and header

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import Alert from "./Alert.js";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { qs, updateCartCount } from "./utils.mjs";
+import { loadHeaderFooter } from './utils.mjs';
+
 
 // Create an instance of Alert
 new Alert();
@@ -18,5 +20,12 @@ const productList = new ProductList("tents", dataSource, listElement);
 // Initialize and render the product list
 productList.init();
 
+
 // Update cart count on page load
 updateCartCount();
+
+// Call load and footer
+loadHeaderFooter();
+
+
+
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -57,4 +57,38 @@ export function updateCartCount() {
     cartCountElement.textContent = "0";
     cartCountElement.style.display = "none";
   }
+
+  export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+}
+
+// for header and footnote
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+// loadTemplate
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerHTML = await loadTemplate('/partials/header.html');
+  const footerHTML = await loadTemplate('/partials/footer.html');
+
+  const headerElement = document.getElementById('main-header');
+  const footerElement = document.getElementById('main-footer');
+
+  renderWithTemplate(headerHTML, headerElement);
+  renderWithTemplate(footerHTML, footerElement);
 }
